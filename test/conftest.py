b101bb914b74bed9b391adeb6536c18331f5dace
GwonilJoo <gwonil3256@gmail.com>
2024-02-28 15:38:17 +0900

    add use case

---
+++ test/conftest.py
@@ -0,0 +1,20 @@
+import uuid
+import random
+from typing import List
+
+from src.domain.room import Room
+
+
+class Fixture:
+    @staticmethod
+    def domain_rooms(num_of_rooms: int) -> List[Room]:
+        return [
+            Room(
+                code=uuid.uuid4(),
+                size=random.randint(50, 500),
+                price=random.randint(10, 100),
+                longitude=random.random(),
+                latitude=random.random(),
+            )
+            for _ in range(num_of_rooms)
+        ]
